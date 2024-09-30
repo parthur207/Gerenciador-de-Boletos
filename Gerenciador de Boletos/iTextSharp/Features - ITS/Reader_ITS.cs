4854b2258c1c79d9daeb8fc82a2cb104062d8135
Paulo Andrade <parthur207@gmail.com>
2024-09-29 23:20:46 -0300

    ok

--- Gerenciador de Boletos/iTextSharp/Features - ITS/Reader_ITS.cs
+++ Gerenciador de Boletos/iTextSharp/Features - ITS/Reader_ITS.cs
@@ -4,13 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Gerenciador_de_Boletos.IO.Features___IO;
+
 
 namespace Gerenciador_de_Boletos.iTextSharp.Directory___Funcions
 {
-    internal class Reader_ITS: IDirectory_ITS
+    internal class Reader_ITS: CreateDirectory, IDirectory_ITS
     {
+        bool validacao_reader = true;
+        public string conteudo_pdf { get; protected set; }
 
-        public void Reader_PDF() { }
+        protected string Path;
+        public void Reader_PDF()
+        {
+            Path = GetPath();
+
+
+        }
 
 
     }
